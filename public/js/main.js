8f915d4cadc79c7b7b138ddbcad99750520072fa
jahangir4csit <jahangir4csit@gmail.com>
2022-12-15 08:35:49 +0600

    landing page

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+/**** Touggle Class category nav */
+
+// function toggleClass(el, className) {
+//     var el = document.querySelectorAll(el);
+
+     
+//      for (i = 0; i < el.length; i++) {
+     
+//        if (el[i].classList) {
+//          el[i].classList.toggle(className);
+//        } else {
+//            var classes = el[i].className.split(' ');
+//            var existingIndex = -1;
+//            for (var j = classes.length; j--;) {
+//              if (classes[j] === className)
+//                existingIndex = j;
+//            }
+   
+//            if (existingIndex >= 0)
+//              classes.splice(existingIndex, 1);
+//            else
+//              classes.push(className);
+   
+//          el[i].className = classes.join(' ');
+//        }
+//      }
+
+
+//    }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/*.html", './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        'beeda': {
+          primary: '#163BDE',
+          secondary: '#FFEF04',
+          neutral: '#2E3A59',
+          inverted: '#C4C4C4',
+          tertiary: '#979797',
+          neonblue: '#CED7FF',
+          gray: '#2F2F2F',
+          link: '#2F80ED',
+        }
+      }
+    }
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+    require('tw-elements/dist/plugin')
+  ],
+}
